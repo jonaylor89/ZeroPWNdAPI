2c46748857fa2a962920f14be2dfc9edf7d9bbc0
John Naylor <jonaylor89@gmail.com>
2018-09-22 21:12:50 -0400

    Adding api

--- hello/views.py
+++ hello/views.py
@@ -5,12 +5,24 @@ from .models import Greeting
 
 import requests
 
+
 # Create your views here.
 def index(request):
-    r = requests.get('http://httpbin.org/status/418')
+    params = {'apikey': '88a3fcdbe688d5928b05d668797d6b0da729e1f403909e12d2b15ca36ff12a99',
+              'resource': 'http://www.virustotal.com'}
+    headers = {
+        "Accept-Encoding": "gzip, deflate",
+        "User-Agent": "ZeroPWNd"
+    }
 
-    print(r.text)
-    return HttpResponse('<pre>' + r.text + '</pre>')
+    _ = requests.post('https://www.virustotal.com/vtapi/v2/file/scan',
+                      params=params,
+                      headers=headers).json()
+    response = requests.get('https://www.virustotal.com/vtapi/v2/file/scan',
+                            params=params,
+                            headers=headers).json()
+    print(response.text)
+    return HttpResponse(response)
 
 
 def db(request):
